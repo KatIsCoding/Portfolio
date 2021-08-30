9d1759d5e93016091d452360792be01805ef7752
KatIsCoding <fabrigoru@gmail.com>
2021-08-30 14:42:22 -0600

    Add Mobile Version Menu

---
+++ index.js
@@ -0,0 +1,10 @@
+function showMenu() {
+    var menu = document.getElementsByClassName("menu")[0];
+    menu.style.height = "100%";
+}
+
+function closeMenu(){
+    var menu = document.getElementsByClassName("menu")[0];
+    menu.style.height = "0";
+    
+}
