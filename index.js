b7a7bac57f2db27a332a3937026fde1c57d51673
KatIsCoding <fabrigoru@gmail.com>
2021-08-30 16:01:09 -0600

    Add js linters and linters fixes

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 function showMenu() {
-    var menu = document.getElementsByClassName("menu")[0];
-    menu.style.height = "100%";
+  const menu = document.getElementsByClassName('menu')[0];
+  menu.style.height = '100%';
 }
 
-function closeMenu(){
-    var menu = document.getElementsByClassName("menu")[0];
-    menu.style.height = "0";
-    
+function closeMenu() {
+  const menu = document.getElementsByClassName('menu')[0];
+  menu.style.height = '0';
 }
+
+// To fix "no-unused-vars" error, however, is there a more intelligent way of doing this?
+showMenu();
+closeMenu();
