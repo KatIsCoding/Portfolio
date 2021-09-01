8b8ccd8be2bbdaf429171b9c77b076115a78bb03
Snow!! <fabrigoru@gmail.com>
2021-09-01 10:11:17 -0600

    Fixes Added

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ const WORKOBJECT = {
   template: {
     name: 'Keeping track of hundreds of components',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    image: 'src/socials/github.svg',
+    image: 'src/works/templateDesktop.svg',
+    imageMobile: 'src/works/templateDesktop.svg',
     technologies: ['Ruby on Rails', 'CSS', 'Javascript', 'HTML'],
     live: 'https://www.google.com',
     source: 'https://www.google.com',
@@ -22,7 +23,8 @@ const WORKOBJECT = {
   template2: {
     name: 'Keeping track of hundreds of components',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    image: 'src/socials/github.svg',
+    image: 'src/works/templateDesktop.svg',
+    imageMobile: 'src/works/templateDesktop.svg',
     technologies: ['Ruby on Rails', 'CSS', 'Javascript', 'HTML'],
     live: 'https://www.google.com',
     source: 'https://www.google.com',
@@ -30,7 +32,8 @@ const WORKOBJECT = {
   template3: {
     name: 'Keeping track of hundreds of components',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    image: 'src/socials/github.svg',
+    image: 'src/works/templateDesktop.svg',
+    imageMobile: 'src/works/templateDesktop.svg',
     technologies: ['Ruby on Rails', 'CSS', 'Javascript', 'HTML'],
     live: 'https://www.google.com',
     source: 'https://www.google.com',
@@ -38,7 +41,8 @@ const WORKOBJECT = {
   template4: {
     name: 'Keeping track of hundreds of components',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    image: 'src/socials/github.svg',
+    image: 'src/works/templateDesktop.svg',
+    imageMobile: 'src/works/templateDesktop.svg',
     technologies: ['Ruby on Rails', 'CSS', 'Javascript', 'HTML'],
     live: 'https://www.google.com',
     source: 'https://www.google.com',
@@ -46,7 +50,8 @@ const WORKOBJECT = {
   template5: {
     name: 'Keeping track of hundreds of components',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    image: 'src/socials/github.svg',
+    image: 'src/works/templateDesktop.svg',
+    imageMobile: 'src/works/templateDesktop.svg',
     technologies: ['Ruby on Rails', 'CSS', 'Javascript', 'HTML'],
     live: 'https://www.google.com',
     source: 'https://www.google.com',
@@ -54,81 +59,93 @@ const WORKOBJECT = {
   template6: {
     name: 'Keeping track of hundreds of components',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    image: 'src/socials/github.svg',
+    image: 'src/works/templateDesktop.svg',
+    imageMobile: 'src/works/templateDesktop.svg',
     technologies: ['Ruby on Rails', 'CSS', 'Javascript', 'HTML'],
     live: 'https://www.google.com',
     source: 'https://www.google.com',
   },
 };
 
-function toggleModal(workId) {
+function toggleBackground() {
   const modal = document.getElementsByClassName('modal')[0];
-  const modalcontainer = document.getElementsByClassName('modal-container')[0];
-  function toggleBackground() {
-    const elements = [document.getElementsByClassName('introduction')[0], document.getElementsByTagName('main')[0], document.getElementsByClassName('aboutmeHeader')[0]];
-    Array.prototype.forEach.call(elements, (elem) => {
-      if (modal.style.display === 'flex') {
-        elem.style.background = '#C1C7D0';
-        elem.style.filter = 'blur(2px)';
-        document.body.style.background = '#C1C7D0';
-      } else {
-        elem.style.background = '';
-        elem.style.filter = 'blur(0)';
-        document.body.style.background = '';
-      }
-    });
-  }
-  function populateModal() {
-    function searchID(id) {
-      return document.getElementById(id);
+  const elements = [document.getElementsByClassName('introduction')[0], document.getElementsByTagName('main')[0], document.getElementsByClassName('aboutmeHeader')[0]];
+  Array.prototype.forEach.call(elements, (elem) => {
+    if (modal.style.display === 'flex') {
+      elem.style.background = '#C1C7D0';
+      elem.style.filter = 'blur(2px)';
+      document.body.style.background = '#C1C7D0';
+    } else {
+      elem.style.background = '';
+      elem.style.filter = 'blur(0)';
+      document.body.style.background = '';
     }
-    const elemData = WORKOBJECT[workId];
-    const workName = searchID('workName');
-    const image = searchID('modal-image');
-    const techs = searchID('modal-technologies');
-    techs.innerHTML = '';
-    const desc = searchID('modal-description');
-    // const demoButton = searchID('live-demo');
-    // const sourceButton = searchID('source');
+  });
+}
+function populateModal(workId) {
+  function searchID(id) {
+    return document.getElementById(id);
+  }
+  const elemData = WORKOBJECT[workId];
+  const workName = searchID('workName');
+  const image = searchID('modal-image');
+  const techs = searchID('modal-technologies');
+  techs.innerHTML = '';
+  const desc = searchID('modal-description');
+  // const demoButton = searchID('live-demo');
+  // const sourceButton = searchID('source');
 
-    workName.innerHTML = elemData.name;
+  workName.innerHTML = elemData.name;
 
+  if (window.innerWidth >= 1000) {
     image.src = elemData.image;
-
-    elemData.technologies.forEach((elem) => {
-      const li = document.createElement('li');
-      li.appendChild(document.createTextNode(elem));
-      techs.appendChild(li);
-    });
-
-    desc.innerHTML = elemData.description;
-
-    // demoButton.href = elemData.live;
-    // sourceButton.href = elemData.source;
-  }
-
-  // background.style.background = "#C1C7D0";
-  if (modal.style.display !== 'flex') {
-    populateModal(workId);
-    modal.style.display = 'flex';
-    modalcontainer.style.display = 'flex';
-    modal.style.opacity = '1';
-    modalcontainer.style.opacity = '1';
   } else {
-    modal.style.display = 'none';
-    modal.style.opacity = '';
-    modalcontainer.style.opacity = '';
-    modalcontainer.style.display = 'none';
+    image.src = elemData.imageMobile;
   }
 
-  toggleBackground();
+  elemData.technologies.forEach((elem) => {
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(elem));
+    techs.appendChild(li);
+  });
+
+  desc.innerHTML = elemData.description;
+
+  // demoButton.href = elemData.live;
+  // sourceButton.href = elemData.source;
+}
+
+function toggleModal(workId) {
+  // This is not to avoid getting errors in code revision
+  // but to avoid the modal opening when the page is loaded
+  try {
+    const modal = document.getElementsByClassName('modal')[0];
+    const modalcontainer = document.getElementsByClassName('modal-container')[0];
+
+    if (modal.style.display !== 'flex') {
+      populateModal(workId);
+      modal.style.display = 'flex';
+      modalcontainer.style.display = 'flex';
+      modal.style.opacity = '1';
+      modalcontainer.style.opacity = '1';
+    } else {
+      modal.style.display = 'none';
+      modal.style.opacity = '';
+      modalcontainer.style.opacity = '';
+      modalcontainer.style.display = 'none';
+    }
+
+    toggleBackground();
+  } catch {
+    return 0;
+  }
+  return 1;
 }
 
 function assignEventHandlers() {
   const workcards = document.getElementsByClassName('workCard');
   Array.prototype.forEach.call(workcards, (elem) => {
     const workId = elem.id;
-
     elem.getElementsByClassName('workInfo')[0].getElementsByTagName('button')[0].addEventListener('click', () => {
       toggleModal(workId);
     }, false);
@@ -136,7 +153,11 @@ function assignEventHandlers() {
 }
 
 // To fix "no-unused-vars" error, however, is there a more intelligent way of doing this?
-exports.showMenu = showMenu;
-exports.closemenu = closeMenu;
-exports.toggleModal = toggleModal;
-exports.assignEventHandlers = assignEventHandlers;
+// Yes there it is ;)
+
+document.onreadystatechange = () => {
+  toggleModal('null');
+  assignEventHandlers();
+  showMenu();
+  closeMenu();
+};
